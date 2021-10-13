97d38899cb95245cf2432f4e2d7d490ccd5cad66
wozien <571455773@qq.com>
2021-10-13 10:46:42 +0800

    template: priority queue

--- templates/__test__/priority-queue.spec.js
+++ templates/__test__/priority-queue.spec.js
@@ -1,9 +1,81 @@
-const PriorityQueue = require('../priority-queue')
+const { MaxPriorityQueue, MinPriorityQueue } = require('../priority-queue')
 
 describe('priority queue', () => {
-    test('init values', () => {
-        const values = [1, 2, 3]
-        const queue = new PriorityQueue(values)
-        expect(queue.values).toEqual(values)
+    describe('max priority queue', () => {
+        test('test queue size', () => {
+            const queue = new MaxPriorityQueue()
+            expect(queue.isEmpty()).toBeTruthy()
+            queue.enqueue(1)
+            expect(queue.size).toBe(1)
+            expect(queue.isEmpty()).toBeFalsy()
+            queue.dequeue()
+            expect(queue.size).toBe(0)
+            expect(queue.isEmpty()).toBeTruthy()
+        })
+
+        test('correctly enqueue with primer number', () => {
+            const arr = [1, 2, 5, 11]
+            const queue = new MaxPriorityQueue()
+            for(let val of arr) {
+                queue.enqueue(val)
+            }
+            let res = []
+            while(!queue.isEmpty()) {
+                const { element } = queue.dequeue()
+                res.push(element)
+            }
+            expect(res).toEqual([11, 5, 2, 1])
+        })
+
+        test('correctly enqueue ele with priority', () => {
+            const arr = [{val: 1}, {val: 2}, {val: 5}]
+            const queue = new MaxPriorityQueue()
+            for(let ele of arr) {
+                queue.enqueue(ele, ele.val)
+            }
+
+            const { element, priority } = queue.dequeue()
+            expect(element).toBe(arr[2])
+            expect(priority).toBe(5)
+        })
+    })
+
+    describe('min priority queue', () => {
+        test('test queue size', () => {
+            const queue = new MinPriorityQueue()
+            expect(queue.isEmpty()).toBeTruthy()
+            queue.enqueue(1)
+            expect(queue.size).toBe(1)
+            expect(queue.isEmpty()).toBeFalsy()
+            queue.dequeue()
+            expect(queue.size).toBe(0)
+            expect(queue.isEmpty()).toBeTruthy()
+        })
+
+        test('correctly enqueue with primer number', () => {
+            const arr = [11, 2, 5, 1]
+            const queue = new MinPriorityQueue()
+            for(let val of arr) {
+                queue.enqueue(val)
+            }
+            let res = []
+            while(!queue.isEmpty()) {
+                const { element } = queue.dequeue()
+                res.push(element)
+            }
+            expect(res).toEqual([1, 2, 5, 11])
+        })
+
+        test('correctly enqueue ele with priority', () => {
+            const arr = [{val: 5}, {val: 2}, {val: 1}]
+            const queue = new MinPriorityQueue()
+            for(let ele of arr) {
+                queue.enqueue(ele, ele.val)
+            }
+
+            const { element, priority } = queue.dequeue()
+            expect(element).toBe(arr[2])
+            expect(priority).toBe(1)
+        })
     })
 })
--- templates/priority-queue.js
+++ templates/priority-queue.js
@@ -3,17 +3,108 @@
  */
 
 class PriorityQueue {
-    constructor(vals = []) {
-        this.values = [...vals]
+    constructor() {
+        this.nodes = []
+        this.len = 0
     }
 
-    enqueue() {
-        
+    get size() {
+        return this.len
+    }
+
+    enqueue(ele, priority) {
+        if (priority === undefined && isNaN(+ele)) {
+            throw new Error('error priority in element.')
+        }
+        const node = {
+            element: ele,
+            priority: priority ? +priority : +ele
+        }
+        this.nodes[++this.len] = node
+        this._swim(this.len)
     }
 
     dequeue() {
+        if (this.isEmpty()) return null
+        const root = this.nodes[1]
+        this._swap(1, this.len)
+        this.nodes[this.len] = null
+        this.len--
+        this._sink(1)
+        return root
+    }
 
+    isEmpty() {
+        return this.len === 0
+    }
+
+    _swim(ch) {
+        while (ch > 1) {
+            const pt = Math.floor(ch / 2)
+            if (!this._shouldSwap(pt, ch)) break
+            this._swap(pt, ch)
+            ch = pt
+        }
+    }
+
+    _sink(pt) {
+        while (2 * pt <= this.len) {
+            const ch = this._getSwapChild(pt)
+            if (!this._shouldSwap(pt, ch)) break
+            this._swap(pt, ch)
+            pt = ch
+        }
+    }
+
+    _swap(i, j) {
+        if(i == j) return
+        const temp = this.nodes[i]
+        this.nodes[i] = this.nodes[j]
+        this.nodes[j] = temp
+    }
+
+    _shouldSwap(pt, ch) {
+        const p1 = this.nodes[pt].priority
+        const p2 = this.nodes[ch].priority
+        return !this._compare(p1, p2)
+    }
+
+    _getSwapChild(pt) {
+        const left = pt * 2
+        const right = pt * 2 + 1
+
+        if(right <= this.len && this._shouldSwap(left, right))
+            return right
+
+        return left
+    }
+
+    // overwrite method
+    _compare() {
     }
 }
 
-module.exports = PriorityQueue
+class MaxPriorityQueue extends PriorityQueue {
+    constructor() {
+        super()
+    }
+
+    _compare(p1, p2) {
+        return p1 > p2
+    }
+}
+
+class MinPriorityQueue extends PriorityQueue {
+    constructor() {
+        super()
+    }
+
+    _compare(p1, p2) {
+        return p1 < p2
+    }
+}
+
+module.exports = {
+    MaxPriorityQueue,
+    MinPriorityQueue
+}

d9237a521688811e7b3d6aae4dd747578e666155
wozien <571455773@qq.com>
2021-10-25 01:08:16 +0800

    lc: array

--- array/560.和为-k-的子数组.js
+++ array/560.和为-k-的子数组.js
@@ -11,8 +11,16 @@
  * @param {number} k
  * @return {number}
  */
-var subarraySum = function(nums, k) {
-
+ var subarraySum = function(nums, k) {
+    let sum = 0, res = 0, countMap = {}
+    countMap[0] = 1
+    for(let i = 0; i < nums.length; i++) {
+        sum += nums[i]
+        let need = sum - k
+        if(countMap[need] !== undefined) res += countMap[need]
+        countMap[sum] = (countMap[sum] || 0) + 1
+    }
+    return res
 };
 // @lc code=end
 
